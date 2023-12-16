55bc9f0df07d100a710a69e03f0ced0ef8f1343d
Your Name <you@example.com>
2023-12-15 20:35:22 -0500

    adding functions to control different LEDS with a button

--- JLearning/FirstApp/main/main.c
+++ JLearning/FirstApp/main/main.c
@@ -1,29 +1,102 @@
 // Libraries to use
+
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include <driver/gpio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 // Definition of GPIO ports
-#define Pulsador 18
-#define Led 33
-#define LedON 1
-#define LedOFF 0
-// Main program
-void app_main(void)
+
+#define button 19
+#define led1 33
+#define led2 25
+#define led3 26
+#define ledOFF 0
+#define ledON 1
+#define cled 2
+
+// local variable
+int currentLed = 0; // Variable para mantener el estado actual del LED
+/*
+function to set pins
+*/
+void SetPines()
 {
-    // pins configuration
-    gpio_set_direction(Pulsador, GPIO_MODE_INPUT);
-    gpio_set_direction(Led, GPIO_MODE_OUTPUT);
-    gpio_set_pull_mode(Pulsador, GPIO_PULLUP_ONLY);
-    gpio_set_level(Led, LedON); //
-    bool boton_presionado = false;
+    gpio_set_direction(button, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(button, GPIO_PULLUP_ONLY);
+    gpio_set_direction(led1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(led2, GPIO_MODE_OUTPUT);
+    gpio_set_direction(led3, GPIO_MODE_OUTPUT);
+    gpio_set_direction(cled, GPIO_MODE_OUTPUT);
+}
+/*
+function in charge to blink the LED (dosnt work)
+*/
+void blinkLed(int pin)
+{
+    bool blinking = true;
+    while (blinking)
+    {
+        int estadoButton = gpio_get_level(button);
+        gpio_set_level(pin, ledON);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+        gpio_set_level(pin, ledOFF);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+        if (estadoButton == 0)
+        {
+            blinking = false;
+        }
+    }
+}
+/*
+to control the interruption of my button
+*/
+void turnOnIndicatorLed(int pin, TickType_t delayTime)
+{
+    gpio_set_level(pin, ledON);
+    vTaskDelay(delayTime);
+    gpio_set_level(pin, ledOFF);
+}
+/*
+function to change between leds
+ */
+void changeLed(void *pvParameters)
+{
     while (true)
     {
-        // To obtain the state of the button
-        int EstadoPulsador = gpio_get_level(Pulsador);
-        //  To define Led's state, with the condition of the button's state
-        gpio_set_level(Led, EstadoPulsador == 0 ? (boton_presionado = true, LedOFF) : (boton_presionado = false, LedON));
-        vTaskDelay(20 / portTICK_PERIOD_MS);
+        // Cambia al siguiente LED cuando se presiona el botón
+        int estadoButton = gpio_get_level(button);
+        if (estadoButton == 0)
+        {
+            // Enciende el indicador LED durante 2 segundos
+            turnOnIndicatorLed(cled, pdMS_TO_TICKS(1000));
+
+            // Espera un poco para evitar múltiples detecciones del botón
+            vTaskDelay(pdMS_TO_TICKS(100));
+
+            // Cambia al siguiente LED
+            currentLed = (currentLed + 1) % 3;
+        }
+
+        // Enciende el LED correspondiente
+        switch (currentLed)
+        {
+        case 0:
+            blinkLed(led1);
+            break;
+        case 1:
+            blinkLed(led2);
+            break;
+        case 2:
+            blinkLed(led3);
+            break;
+        }
     }
+}
+// main app
+void app_main(void)
+{
+    SetPines();
+    xTaskCreate(changeLed, "change_led_task", 2048, NULL, 10, NULL);
 }
