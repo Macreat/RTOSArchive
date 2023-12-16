4be0b9e08766d269a822bfb5c9344df30ce2eb6d
Your Name <you@example.com>
2023-12-15 20:24:16 -0500

    changing to a simple program to change state LED with a button

--- JLearning/FirstApp/main/main.c
+++ JLearning/FirstApp/main/main.c
@@ -1,32 +1,29 @@
-// importing libraries
+// Libraries to use
 #include <stdio.h>
 #include <stdbool.h>
-#include <unistd.h>
 #include <driver/gpio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-// defining constants
-#define Button 18
+// Definition of GPIO ports
+#define Pulsador 18
 #define Led 33
-#define portTICK_PERIOD_MS 1
 #define LedON 1
 #define LedOFF 0
-
+// Main program
 void app_main(void)
 {
-    // configuirn pins
-    gpio_set_direction(Button, GPIO_MODE_INPUT);
+    // pins configuration
+    gpio_set_direction(Pulsador, GPIO_MODE_INPUT);
     gpio_set_direction(Led, GPIO_MODE_OUTPUT);
-    gpio_set_pull_mode(Button, GPIO_PULLUP_ONLY);
-    // variable definition
-    bool blinking = true;
-    // main bucle
+    gpio_set_pull_mode(Pulsador, GPIO_PULLUP_ONLY);
+    gpio_set_level(Led, LedON); //
+    bool boton_presionado = false;
     while (true)
     {
-        int buttonState = gpio_get_level(Button);
-        buttonState == 0 ? (blinking = !blinking, vTaskDelay(20 / portTICK_PERIOD_MS))
-                         : (void)0;
-        blinking == 1 ? (gpio_set_level(Led, LedON), vTaskDelay(10 / portTICK_PERIOD_MS), gpio_set_level(Led, LedOFF), vTaskDelay(10 / portTICK_PERIOD_MS))
-                      : gpio_set_level(Led, LedOFF);
+        // To obtain the state of the button
+        int EstadoPulsador = gpio_get_level(Pulsador);
+        //  To define Led's state, with the condition of the button's state
+        gpio_set_level(Led, EstadoPulsador == 0 ? (boton_presionado = true, LedOFF) : (boton_presionado = false, LedON));
+        vTaskDelay(20 / portTICK_PERIOD_MS);
     }
 }
