4b24835e39d452984c02747aead47fc32ea01612
Your Name <you@example.com>
2023-12-17 22:48:19 -0500

    adding necessaries files to second task libraries

---
+++ JLearning/FirstApp/main/ConfigStr.c
@@ -0,0 +1,63 @@
+#include "ConfigStr.h"
+
+void setPins()
+{
+    gpio_set_direction(button, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(button, GPIO_PULLUP_ONLY);
+    gpio_set_direction(led1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(led2, GPIO_MODE_OUTPUT);
+    gpio_set_direction(led3, GPIO_MODE_OUTPUT);
+    gpio_set_direction(cled, GPIO_MODE_OUTPUT);
+}
+/*
+function to configure ADC
+*/
+void setADC()
+{
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(Pot, ADC_ATTEN_DB_11);
+}
+/*
+function to configure PWM Channel
+*/
+void setPWM()
+{
+    // Configuración del canal PWM
+    ledc_timer_config_t timer_conf = {
+        .duty_resolution = PwmResolution,
+        .freq_hz = PwmFreq,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0};
+    ledc_timer_config(&timer_conf);
+
+    // Configuración de los canales PWM
+    for (int i = 0; i < NLeds; i++)
+    {
+        ledc_channel_config_t ledc_conf = {
+            .gpio_num = led1 + i,
+            .speed_mode = LEDC_HIGH_SPEED_MODE,
+            .channel = LEDC_CHANNEL_0 + i,
+            .intr_type = LEDC_INTR_DISABLE,
+            .timer_sel = LEDC_TIMER_0,
+            .duty = 0};
+        ledc_channel_config(&ledc_conf);
+    }
+    ledc_channel_config_t ledc_conf_3 = {
+        .gpio_num = led3,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .channel = LEDC_CHANNEL_2, // Use another channel avaliable
+        .intr_type = LEDC_INTR_DISABLE,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0};
+    ledc_channel_config(&ledc_conf_3);
+}
+/*
+function to use interruption
+
+void turnOnIndicatorLed(int pin, TickType_t delayTime)
+{
+    gpio_set_level(pin, 1);
+    vTaskDelay(delayTime);
+    gpio_set_level(pin, 0);
+}
+*/
---
+++ JLearning/FirstApp/main/ConfigStr.h
@@ -0,0 +1,39 @@
+#ifndef ConfigStructure
+#define ConfigStructure
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "freertos/timers.h"
+#include "driver/adc.h"
+#include "freertos/queue.h"
+#include "driver/uart.h"
+#include "esp_intr_alloc.h"
+#include "driver/ledc.h"
+
+#define Pot ADC1_CHANNEL_7
+#define button 19
+#define led1 25
+#define led2 26
+#define led3 33
+#define cled 2
+#define stackSize 1024
+#define RDelay 500
+#define NLeds 3
+#define PwmFreq 5000
+#define PwmResolution LEDC_TIMER_8_BIT
+#define PwmChannel LEDC_CHANNEL_0
+#define ledON 1
+#define ledOFF 0
+
+// function prototype
+void turnOnIndicatorLed(int pin, TickType_t delayTime);
+void setPins(void);
+void setADC(void);
+void setPWM(void);
+#endif
