013c05d83d3eb54c0a4506da5d1db89633733037
Your Name <you@example.com>
2024-02-18 12:18:08 -0500

    using main exe for a simple programm to sense room temperature using LM35 microchip

--- JLearning/FirstApp/main/main.c
+++ JLearning/FirstApp/main/main.c
@@ -1,16 +1,28 @@
-#include "ConfigStr.h"
-#include "tasks.h"
+// libraries used
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
 
-extern QueueHandle_t ADCLecture;     // Variable is created for the ADC read queue, indicating that it is being taken from an external file
-extern QueueHandle_t SetValuesQueue; // A variable is created for the queue where the limits entered by UART are stored, indicating that it is being taken from an external file
-// main app just controlling state of a different LEDS with an NTC termistor.
-void app_main(void)
+#define LM35_ADC_CHANNEL ADC1_CHANNEL_7 // USING GPIO34 pin
+#define DEFAULT_VREF 1100               // Using generic value for callibration
+
+void app_main()
 {
-    ADCLecture = xQueueCreate(10, sizeof(float));              // The queue of 10 spaces and floating type values ​​is created
-    SetValuesQueue = xQueueCreate(6, sizeof(Dict_set_values)); // The queue of 6 spaces is created with values ​​of type Dict_set_values ​​(STRUCTURE CREATED)
+    // Initializing ADC
+    adc1_config_width(ADC_WIDTH_BIT_12);                          // 12 bits configuration
+    adc1_config_channel_atten(LM35_ADC_CHANNEL, ADC_ATTEN_DB_11); // Atenuation from maximum 3.3 V input
+    while (1)
+    {
+        // read ADC
+        int adc_reading = adc1_get_raw(LM35_ADC_CHANNEL);
 
-    setPins();
-    setADC();
-    initUart();
-    createTask();
+        // Convert ADC lecture to temperature in °C
+        float voltage = (float)adc_reading / 4095.0 * 3.3; // Converting adc lecture to drop voltage (3.3V is reference)
+        float temperature = voltage * 100.0;               // LM35 give 10 mV for °C-
+
+        printf(" room temperature: %.2f°C\n", temperature);
+
+        vTaskDelay(1000 / porTICK_PERIOD_MS); // one second delay
+    }
 }
