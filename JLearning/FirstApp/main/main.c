f89ffc9d06bfe40d9ef79dcaba387aae453f6074
Your Name <you@example.com>
2024-02-18 12:40:15 -0500

    chaning structure using STA and AP from ESP32 to connect over wifi , then found current hour and data time in COLOMBIA using SNTP and sending via UART

--- JLearning/FirstApp/main/main.c
+++ JLearning/FirstApp/main/main.c
@@ -1,17 +1,20 @@
-// Wifi and tcp controller on ESP32 (connect to a network wwhich is connect to a host and send data over TCP )
-//  Importación de librerias utilizadas
-#include <stdio.h>
+// imported libraries
 #include <string.h>
-
-#include <string.h>
+#include <time.h>
+#include <sys/time.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_system.h"
-#include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_attr.h"
+#include "esp_sleep.h"
 #include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "esp_sntp.h"
+#include "esp_wifi.h"
+#include "driver/uart.h"
 
 #include "lwip/err.h"
 #include "lwip/sockets.h"
@@ -19,29 +22,27 @@
 #include "lwip/netdb.h"
 #include "lwip/dns.h"
 
-/** DEFINES **/
-
+// defining constants
+#define UART_NUM UART_NUM_2
+#define BUF_SIZE 1024
+#define STACK_SIZE 1024
 #define WIFI_SUCCESS 1 << 0
 #define WIFI_FAILURE 1 << 1
 #define TCP_SUCCESS 1 << 0
 #define TCP_FAILURE 1 << 1
 #define MAX_FAILURES 10
-#define WIFI_CONNECT_TIMEOUT pdMS_TO_TICKS(10000) // 10-second timeout
 
-/** GLOBALS **/
-
-// event group to contain status information
+// declaration of variables , functions and queues
+static const char *TAG = "example";
+static const char *tag = "UART EVENT";
+static QueueHandle_t uart_queue;
 static EventGroupHandle_t wifi_event_group;
-
-// retry  tracker
 static int s_retry_num = 0;
+static void initialize_sntp(void);
+static void update_time(void);
+static void init_uart(void);
 
-// task tag
-static const char *TAG = "WIFI";
-
-/** FUNCTIONS **/
-
-// prototipado
+esp_err_t connect_wifi("IphMat", "110623Al");
 
 // event handler for wifi events
 static void wifi_event_handler(void *arg, esp_event_base_t event_base,
@@ -56,7 +57,7 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
     {
         if (s_retry_num < MAX_FAILURES)
         {
-            ESP_LOGI(TAG, "Reconnecting top AP...");
+            ESP_LOGI(TAG, "Reconnecting to AP...");
             esp_wifi_connect();
             s_retry_num++;
         }
@@ -68,7 +69,6 @@ static void wifi_event_handler(void *arg, esp_event_base_t event_base,
 }
 
 // event handler for ip events
-
 static void ip_event_handler(void *arg, esp_event_base_t event_base,
                              int32_t event_id, void *event_data)
 {
@@ -81,15 +81,98 @@ static void ip_event_handler(void *arg, esp_event_base_t event_base,
     }
 }
 
-// connect to wifi and return the result
+void time_sync_notification_cb(struct timeval *tv)
+{
+    ESP_LOGW(TAG, "Notification of a time synchronization event");
+}
+
+static void update_time(void)
+{
+    while (1)
+    {
+        // wait for time to be set
+        static time_t now = 0;
+        static struct tm timeinfo = {0};
+
+        // update 'now' variable with current time
+        time(&now);
+
+        char strftime_buf[64];
+
+        // Set timezone to Colombia Standard Time
+        setenv("TZ", "GTM+5", 1);
+        tzset();
+        localtime_r(&now, &timeinfo);
+        strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+        ESP_LOGI(TAG, "The current date/time in Colombia is: %s", strftime_buf);
+        uart_write_bytes(UART_NUM, strcat(strftime_buf, "\n"), strlen(strftime_buf));
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+/*
+main app
+*/
+void app_main(void)
+{
+
+    init_uart();
+
+    esp_err_t status = WIFI_FAILURE;
+
+    // initialize storage
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // connect to wireless AP
+    status = connect_wifi();
+    if (WIFI_SUCCESS != status)
+    {
+        ESP_LOGI(TAG, "Failed to associate to AP, dying...");
+        return;
+    }
+
+    initialize_sntp();
+
+    while (sntp_get_sync_status() != SNTP_SYNC_STATUS_COMPLETED)
+    {
+        ESP_LOGE(TAG, "WAITING for an hour update...");
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+
+    static uint8_t ucParameterToPass;
+    TaskHandle_t xHandle = NULL;
+    xTaskCreate(update_time, "update_time", STACK_SIZE * 3, ucParameterToPass, 5, xHandle);
+}
+/*
+function to initialize sntp or SIMPLE NETWORK TIME PROTOCL for synchronizing the system of device over a network */
+static void initialize_sntp(void)
+{
+    ESP_LOGI(TAG, "Initializing SNTP");
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_set_sync_interval(15000);
+    sntp_setservername(0, "pool.ntp.org");
+    sntp_set_time_sync_notification_cb(time_sync_notification_cb);
+    sntp_init();
+}
+
+/*
+connect to wifi and return the result
+*/
 esp_err_t connect_wifi()
 {
     int status = WIFI_FAILURE;
 
-    /*INITIALIZE ALL THE THINGS, LIKE EVENTS its gonna happen*/
+    /** INITIALIZE ALL THE THINGS **/
     // initialize the esp network interface
     ESP_ERROR_CHECK(esp_netif_init());
-    // initialize default event loop,
+
+    // initialize default esp event loop
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     // create wifi station in the wifi driver
@@ -97,12 +180,12 @@ esp_err_t connect_wifi()
 
     // setup wifi station with the default wifi configuration
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg)); // Correctly pass the config to the init function
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    /**EVENT LOOP CRAZINESS**/
-    wifi_event_group = xEventGroupCreate(); // where the output of the event is going to live, GLOBAL VARIABLE
+    /** EVENT LOOP CRAZINESS **/
+    wifi_event_group = xEventGroupCreate();
 
-    esp_event_handler_instance_t wifi_handler_event_instance; // just checking and call the wifi_event_handler
+    esp_event_handler_instance_t wifi_handler_event_instance;
     ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
                                                         ESP_EVENT_ANY_ID,
                                                         &wifi_event_handler,
@@ -116,9 +199,10 @@ esp_err_t connect_wifi()
                                                         NULL,
                                                         &got_ip_event_instance));
 
+    /** START THE WIFI DRIVER **/
     wifi_config_t wifi_config = {
         .sta = {
-            .ssid = "IphMat", // Here i can put my real stuff
+            .ssid = "IphMat",
             .password = "110623Al",
             .threshold.authmode = WIFI_AUTH_WPA2_PSK,
             .pmf_cfg = {
@@ -133,110 +217,60 @@ esp_err_t connect_wifi()
     // set the wifi config
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
-    // start the wifi driver , hhere the event loop for the wifi controller kicks off
+    // start the wifi driver
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "STA initialization complete");
 
-    /**NOW WE WAIT**/
-    ESP_LOGI(TAG, "Waiting for WiFi connection...");
-
+    /** NOW WE WAIT **/
     EventBits_t bits = xEventGroupWaitBits(wifi_event_group,
                                            WIFI_SUCCESS | WIFI_FAILURE,
                                            pdFALSE,
                                            pdFALSE,
-                                           WIFI_CONNECT_TIMEOUT); // 10-second timeout
-    ESP_LOGI(TAG, "Done waiting for WiFi connection.");
+                                           portMAX_DELAY);
 
-    /* After timeout or event happens, we check which one it was. */
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
     if (bits & WIFI_SUCCESS)
     {
-        ESP_LOGI(TAG, "Succeded connected to AP");
+        ESP_LOGI(TAG, "Connected to ap");
         status = WIFI_SUCCESS;
     }
     else if (bits & WIFI_FAILURE)
     {
-        ESP_LOGI(TAG, "Failed to connect to AP");
+        ESP_LOGI(TAG, "Failed to connect to ap");
         status = WIFI_FAILURE;
     }
     else
     {
-        ESP_LOGE(TAG, "Failed to connect to AP (timeout)");
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
         status = WIFI_FAILURE;
     }
 
     /* The event will not be processed after unregister */
-
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, got_ip_event_instance));
     ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_handler_event_instance));
     vEventGroupDelete(wifi_event_group);
 
     return status;
 }
-
-// connect to the server and return the result
-
-esp_err_t connect_tcp_server(void)
+/*
+Function to configurate uart charactheristics */
+static void init_uart(void)
 {
-    struct sockaddr_in serverInfo = {0};
-    char readBuffer[1024] = {0};
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .parity = UART_PARITY_DISABLE,
+        .source_clk = UART_SCLK_APB,
+        .stop_bits = UART_STOP_BITS_1};
 
-    serverInfo.sin_family = AF_INET;
-    serverInfo.sin_addr.s_addr = htonl(0x8200140a); // Use htonl to ensure proper byte order
-    serverInfo.sin_port = htons(12345);
+    uart_param_config(UART_NUM, &uart_config);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-    {
-        ESP_LOGE(TAG, "Failed to create a socket..?");
-        return TCP_FAILURE;
-    }
+    uart_set_pin(UART_NUM, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
-    if (connect(sock, (struct sockaddr *)&serverInfo, sizeof(serverInfo)) != 0)
-    {
-        ESP_LOGE(TAG, "Faild to connect to %s!", inet_ntoa(serverInfo.sin_addr.s_addr));
-        return TCP_FAILURE;
-    }
+    uart_driver_install(UART_NUM, BUF_SIZE, BUF_SIZE, 5, &uart_queue, 0);
 
-    ESP_LOGI(TAG, "Connected to TCP server");
-    bzero(readBuffer, sizeof(readBuffer));
-    int r = read(sock, readBuffer, sizeof(readBuffer) - 1);
-    for (int i = 0; i < r; i++)
-    {
-        putchar(readBuffer[i]);
-    }
-    if (strcmp(readBuffer, "HELLO") == 0)
-    {
-        ESP_LOGI(TAG, "NICE,CONNECTED DID IT");
-    }
-    return TCP_SUCCESS;
-}
-
-void app_main(void)
-{
-    esp_err_t status = WIFI_FAILURE;
-
-    // initialize sotrage ; nonvoltagie storie
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-    {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-
-    // connect to wireless AP
-    status = connect_wifi();
-    if (WIFI_SUCCESS != status)
-    {
-        ESP_LOGI(TAG, "Failed to associate to AP, exiting...");
-        return;
-    }
-
-    status = connect_tcp_server();
-    if (TCP_SUCCESS != status)
-    {
-        ESP_LOGI(TAG, "Failed to connect to remote server, exiting...");
-        return;
-    }
+    ESP_LOGI(tag, "Init uart completed");
 }
